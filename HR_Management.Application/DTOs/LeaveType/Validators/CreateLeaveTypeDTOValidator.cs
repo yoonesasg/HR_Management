7d9ac4e1bcd175d3fb917228e466e53b274e79ea
Yoones <yoonesasgasg@gmail.com>
2024-11-16 00:00:32 +0330

    افزودن fluentValidation

---
+++ HR_Management.Application/DTOs/LeaveType/Validators/CreateLeaveTypeDTOValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace HR_Management.Application.DTOs.LeaveType.Validators
+{
+    public class CreateLeaveTypeDTOValidator : AbstractValidator<CreateLeaveTypeDTO>
+    {
+        public CreateLeaveTypeDTOValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName} is Required")
+                .NotNull();
+            RuleFor(x => x.DefaultDay).NotEmpty().WithMessage("{ PropertyName} is Required").NotNull();
+        }
+    }
+}
---
+++ HR_Management.Application/Features/LeaveAllocations/Requests/Commands/DeleteLeaveAllocationCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace HR_Management.Application.Features.LeaveAllocations.Requests.Commands
+{
+    public class DeleteLeaveAllocationCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
