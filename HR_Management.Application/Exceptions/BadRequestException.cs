f87c838f09156ecd08ce78d529810bf4f19cd14a
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-07 21:23:58 +0330

    14031018 : add custom exceptions : 33

---
+++ HR_Management.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HR_Management.Application.Exceptions
+{
+    public class BadRequestException : ApplicationException
+    {
+        public BadRequestException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ HR_Management.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HR_Management.Application.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string name, object key) : base ($"{name} ({key} Was Not Found!)")
+        {
+
+        }
+    }
+}
