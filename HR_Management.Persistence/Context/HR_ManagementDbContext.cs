2f158bfbb85447667d3d59f32e93618e3885739f
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-09 15:06:54 +0330

    14031020 : add override methods in dbcontext

--- HR_Management.Persistence/Context/HR_ManagementDbContext.cs
+++ HR_Management.Persistence/Context/HR_ManagementDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using HR_Management.Domain;
+using HR_Management.Domain.Common;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HR_Management.Persistence.Context
 {
@@ -12,8 +16,47 @@ namespace HR_Management.Persistence.Context
         }
         #endregion
 
+        #region DbSet
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        #endregion
+
+        #region OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HR_ManagementDbContext).Assembly);
+        }
+        #endregion
+
+        #region SaveChanges
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach(var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if(entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        }
+        #endregion
     }
 }
