1a63477941df41a67c70d293694ef08077188e92
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-09 14:53:48 +0330

    14031020 : add entity and dbcontext : 38

---
+++ HR_Management.Persistence/Context/HR_ManagementDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HR_Management.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management.Persistence.Context
+{
+    public class HR_ManagementDbContext : DbContext
+    {
+        #region Constructor
+        public HR_ManagementDbContext(DbContextOptions<HR_ManagementDbContext> options) : base(options)
+        {
+
+        }
+        #endregion
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    }
+}
