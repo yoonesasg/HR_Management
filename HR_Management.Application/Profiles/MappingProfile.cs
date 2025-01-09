52c2d3d35aa5bd69c33df219b61ec2daed5adbe7
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-09 14:30:02 +0330

    14031020 : update mapping profile : 36

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -10,9 +10,24 @@ namespace HR_Management.Application.Profiles
     {
         public MappingProfile()
         {
+            #region LeaveRequest
             CreateMap<LeaveRequest, LeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDTO>().ReverseMap();
+            #endregion
+
+            #region LeaveAllocation
             CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDTO>().ReverseMap();
+            #endregion
+
+            #region LeaveType
             CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveType, UpdateLeaveTypeDTO>().ReverseMap();
+            #endregion
         }
     }
 }
