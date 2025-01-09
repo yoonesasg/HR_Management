8c1f3838d0c54eda347f2c617ba04d36ea955331
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-09 15:31:48 +0330

    14031020 : add generic repository of persistence

--- HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
+++ HR_Management.Application/Persistence/Contracts/IGenericRepository.cs
@@ -6,10 +6,10 @@ namespace HR_Management.Application.Persistence.Contracts
     public interface IGenericRepository<T> where T : class
     {
         Task<T> Get(int id);
-        Task<T> Delete(int id);
-        Task<T> Delete(T entity);
+        Task Delete(int id);
+        Task Delete(T entity);
         Task<IReadOnlyList<T>> GetAll();
-        Task<T> Update(T entity);
+        Task Update(T entity);
         Task<T> Add(T entity);
         Task<bool> Exist(int id);
     }
---
+++ HR_Management.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using HR_Management.Application.Persistence.Contracts;
+using HR_Management.Persistence.Context;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HR_Management.Persistence.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly HR_ManagementDbContext _context;
+        public GenericRepository(HR_ManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> Add(T entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Exist(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<T> Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Update(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
