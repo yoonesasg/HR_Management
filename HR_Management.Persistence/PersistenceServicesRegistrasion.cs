140eb3097a79b9dab394be8ae93558e0a133584d
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-10 11:24:25 +0330

    14031021 : add service registration to persistence : 42

---
+++ HR_Management.Persistence/PersistenceServicesRegistrasion.cs
@@ -0,0 +1,37 @@
+﻿using HR_Management.Application.Persistence.Contracts;
+using HR_Management.Persistence.Context;
+using HR_Management.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR_Management.Persistence
+{
+    public static class PersistenceServicesRegistrasion
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            #region DbContext
+
+            services.AddDbContext<HR_ManagementDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("HR_ManagementConnectionString"));
+            });
+
+            #endregion
+
+            #region Dependency Injection
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+
+            #endregion
+
+            return services;
+
+        }
+    }
+}
