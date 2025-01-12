cd20b8b5355e200d20080b7269bb155a00b3d282
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-12 23:20:27 +0330

    14032023 : add email sender to infra layer

---
+++ HR_Management.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using HR_Management.Application.Contracts.Infrastructure;
+using HR_Management.Application.Models;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace HR_Management.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSetting _emailSetting;
+
+        public EmailSender(IOptions<EmailSetting> emailSetting)
+        {
+            _emailSetting = emailSetting.Value;
+        }
+
+        public async Task<bool> SendEmail(Email email)
+        {
+            var client = new SendGridClient(_emailSetting.APIKey);
+            var to = new EmailAddress(email.To);
+            var from = new EmailAddress
+            {
+                Email = _emailSetting.FromAddress,
+                Name = _emailSetting.FromName
+            };
+            var message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+            var responce = await client.SendEmailAsync(message);
+            return responce.StatusCode == HttpStatusCode.Accepted || responce.StatusCode == HttpStatusCode.OK;
+        }
+    }
+}
