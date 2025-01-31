0f52c82ca56a62b60424fa1b0568d6136925a122
Yoones Asgarian <yoonesasgasg@gmail.com>
2025-01-31 15:12:06 +0330

    14031112 : 78 : role

---
+++ HR_Management.Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR_Management.Identity.Configurations
+{
+    internal class UserRoleConfiguration
+    {
+    }
+}
--- HR_Management.Identity/HR_ManagementIdentityDbContext.cs
+++ HR_Management.Identity/HR_ManagementIdentityDbContext.cs
@@ -1,9 +1,21 @@
-﻿using HR_Management.Identity.Models;
+﻿using HR_Management.Identity.Configurations;
+using HR_Management.Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace HR_Management.Identity
 {
-    internal class HR_ManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
+    public class HR_ManagementIdentityDbContext : IdentityDbContext<ApplicationUser>
     {
+        public HR_ManagementIdentityDbContext(DbContextOptions<HR_ManagementIdentityDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new RoleConfiguration());
+        }
     }
 }
